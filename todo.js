d62563d2969c30a310e6e5d55fe5476b31d00daa
sami-sad-ik <sadiksami530@gmail.com>
2025-10-10 20:53:22 +0600

    todo list app project done

---
+++ todo.js
@@ -0,0 +1,51 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const loadTasks = () => {
+  const container = document.getElementById("listContainer");
+  container.innerHTML = "";
+  tasks.forEach((task, index) => {
+    const taskDiv = document.createElement("div");
+    taskDiv.innerHTML = `
+        <div class="flex justify-start items-center gap-2">
+            <input type="checkbox" ${
+              task.completed ? "checked" : ""
+            } class="completedTask appearance-none w-4 h-4 border-2 border-gray-500 rounded-full cursor-pointer transition-all
+                    duration-200 checked:bg-red-600 checked:border-red-600 relative" />
+            <p class="task-list ${task.completed ? "completed" : ""}">${
+      task.text
+    }</p>
+            <button class="hover:text-red-500 transition-colors duration-150">
+                <i class="fa-regular fa-circle-xmark"></i>
+            </button>
+        </div>
+    `;
+    const checkbox = taskDiv.querySelector(".completedTask");
+    const taskText = taskDiv.querySelector(".task-list");
+    checkbox.addEventListener("change", () => {
+      tasks[index].completed = checkbox.checked;
+      if (checkbox.checked) {
+        taskText.classList.add("completed");
+      } else taskText.classList.remove("completed");
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    });
+
+    container.appendChild(taskDiv);
+    const deleteButton = taskDiv.querySelector("button");
+    deleteButton.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      loadTasks();
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+const addTask = () => {
+  const taskText = document.getElementById("todoInput").value.trim();
+  if (!taskText) return;
+  const content = { text: taskText, completed: false };
+  tasks.push(content);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  loadTasks();
+};
